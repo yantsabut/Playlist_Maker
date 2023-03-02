23d72c46485fc204a31255a05b05d42566642e50
Yan <yantsabut777@yandex.ru>
2023-03-02 20:38:31 +0300

    add third commit

--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -1,11 +1,17 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val buttonSettingsBack = findViewById<View>(R.id.settings_back_button)
+        buttonSettingsBack.setOnClickListener {
+            finish()
+        }
     }
 }
