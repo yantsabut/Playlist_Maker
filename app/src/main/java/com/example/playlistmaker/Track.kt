92f45960bb547f179ca2fb82ac39870940a56970
Yan <yantsabut777@yandex.ru>
2023-05-31 23:15:03 +0300

    add commit( домашняя работа по спринту №14)

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -11,6 +11,7 @@ data class Track(
     val collectionName: String?,
     val releaseDate: String,
     val primaryGenreName: String,
-    val country: String
+    val country: String,
+    val previewUrl: String
 )
 
